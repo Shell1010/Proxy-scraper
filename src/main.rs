045dc8d14d485caee808e45b2e74e536c48d3409
Shell <72198971+Shell1010@users.noreply.github.com>
2022-12-01 20:26:49 +0000

    Added more sources

--- src/main.rs
+++ src/main.rs
@@ -2,15 +2,53 @@ mod scrapers;
 use futures::stream::futures_unordered::FuturesUnordered;
 use futures::StreamExt;
 use scrapers::Scraper;
-use tokio::main;
+use tokio::{main, join};
 #[main]
 async fn main() {
-    http_scrape().await;
-    socks_scrape().await;
+    join!(http_scrape(), socks_scrape());
 }
 async fn http_scrape() {
     let http_urls = [
+        "https://openproxy.space/list/http",
         "https://spys.me/proxy.txt",
+        "https://openproxylist.xyz/http.txt",
+        "https://proxyscan.io/download?type=http",
+        "https://proxysearcher.sourceforge.net/Proxy%20List.php?type=http",
+        "https://proxyspace.pro/http.txt",
+        "https://proxyspace.pro/https.txt",
+        "https://raw.githubusercontent.com/almroot/proxylist/master/list.txt",
+        "https://raw.githubusercontent.com/andigwandi/free-proxy/main/proxy_list.txt",
+        "https://raw.githubusercontent.com/aslisk/proxyhttps/main/https.txt",
+        "https://raw.githubusercontent.com/B4RC0DE-TM/proxy-list/main/HTTP.txt",
+        "https://raw.githubusercontent.com/hanwayTech/free-proxy-list/main/http.txt",
+        "https://raw.githubusercontent.com/hanwayTech/free-proxy-list/main/https.txt",
+        "https://raw.githubusercontent.com/hendrikbgr/Free-Proxy-Repo/master/proxy_list.txt",
+        "https://raw.githubusercontent.com/HyperBeats/proxy-list/main/http.txt",
+        "https://raw.githubusercontent.com/jetkai/proxy-list/main/online-proxies/txt/proxies-http.txt",
+        "https://raw.githubusercontent.com/jetkai/proxy-list/main/online-proxies/txt/proxies-https.txt",
+        "https://raw.githubusercontent.com/mertguvencli/http-proxy-list/main/proxy-list/data.txt",
+        "https://raw.githubusercontent.com/mmpx12/proxy-list/master/http.txt",
+        "https://raw.githubusercontent.com/mmpx12/proxy-list/master/https.txt",
+        "https://raw.githubusercontent.com/ObcbO/getproxy/master/http.txt",
+        "https://raw.githubusercontent.com/ObcbO/getproxy/master/https.txt",
+        "https://raw.githubusercontent.com/proxy4parsing/proxy-list/main/http.txt",
+        "https://raw.githubusercontent.com/proxylist-to/proxy-list/main/http.txt",
+        "https://raw.githubusercontent.com/roosterkid/openproxylist/main/HTTPS_RAW.txt",
+        "https://raw.githubusercontent.com/rx443/proxy-list/main/online/http.txt",
+        "https://raw.githubusercontent.com/rx443/proxy-list/main/online/https.txt",
+        "https://raw.githubusercontent.com/saisuiu/uiu/main/free.txt",
+        "https://raw.githubusercontent.com/saschazesiger/Free-Proxies/master/proxies/http.txt",
+        "https://raw.githubusercontent.com/ShiftyTR/Proxy-List/master/http.txt",
+        "https://raw.githubusercontent.com/ShiftyTR/Proxy-List/master/https.txt",
+        "https://raw.githubusercontent.com/sunny9577/proxy-scraper/master/proxies.txt",
+        "https://raw.githubusercontent.com/TheSpeedX/PROXY-List/master/http.txt",
+        "https://raw.githubusercontent.com/yemixzy/proxy-list/main/proxy-list/data.txt",
+        "https://raw.githubusercontent.com/Zaeem20/FREE_PROXIES_LIST/master/http.txt",
+        "https://raw.githubusercontent.com/Zaeem20/FREE_PROXIES_LIST/master/https.txt",
+        "https://raw.githubusercontent.com/zevtyardt/proxy-list/main/http.txt",
+        "https://rootjazz.com/proxies/proxies.txt",
+        "https://sheesh.rip/http.txt",
+        "https://www.freeproxychecker.com/result/http_proxies.txt",
         "https://api.proxyscrape.com/?request=getproxies&proxytype=http&timeout=1000&country=All",
         "https://www.proxy-list.download/api/v1/get?type=http&anon=elite",
         "https://www.proxy-list.download/api/v1/get?type=http&anon=transparent",
@@ -65,6 +103,26 @@ async fn socks_scrape() {
     let client = reqwest::Client::new();
 
     let socks_urls = [
+        "https://openproxy.space/list/socks4",
+        "https://openproxylist.xyz/socks4.txt",
+        "https://proxyscan.io/download?type=socks4",
+        "https://proxyscan.io/download?type=socks4",
+        "https://proxysearcher.sourceforge.net/Proxy%20List.php?type=socks",
+        "https://proxyspace.pro/socks4.txt",
+        "https://raw.githubusercontent.com/B4RC0DE-TM/proxy-list/main/SOCKS4.txt",
+        "https://raw.githubusercontent.com/hanwayTech/free-proxy-list/main/socks4.txt",
+        "https://raw.githubusercontent.com/HyperBeats/proxy-list/main/socks4.txt",
+        "https://raw.githubusercontent.com/jetkai/proxy-list/main/online-proxies/txt/proxies-socks4.txt",
+        "https://raw.githubusercontent.com/mmpx12/proxy-list/master/socks4.txt",
+        "https://raw.githubusercontent.com/ObcbO/getproxy/master/socks4.txt",
+        "https://raw.githubusercontent.com/proxylist-to/proxy-list/main/socks4.txt",
+        "https://raw.githubusercontent.com/roosterkid/openproxylist/main/SOCKS4_RAW.txt",
+        "https://raw.githubusercontent.com/saschazesiger/Free-Proxies/master/proxies/socks4.txt",
+        "https://raw.githubusercontent.com/ShiftyTR/Proxy-List/master/socks4.txt",
+        "https://raw.githubusercontent.com/TheSpeedX/PROXY-List/master/socks4.txt",
+        "https://raw.githubusercontent.com/Zaeem20/FREE_PROXIES_LIST/master/socks4.txt",
+        "https://raw.githubusercontent.com/zevtyardt/proxy-list/main/socks4.txt",
+        "https://www.freeproxychecker.com/result/socks4_proxies.txt",
         "https://spys.me/socks.txt",
         "https://api.proxyscrape.com/?request=getproxies&proxytype=socks&timeout=1000&country=All",
         "https://www.proxy-list.download/api/v1/get?type=socks4&anon=elite",
@@ -73,6 +131,27 @@ async fn socks_scrape() {
         "https://www.proxy-list.download/api/v1/get?type=socks5&anon=elite",
         "https://www.proxy-list.download/api/v1/get?type=socks5&anon=transparent",
         "https://www.proxy-list.download/api/v1/get?type=socks5&anon=anonymous",
+        "https://openproxy.space/list/socks5",
+        "https://openproxylist.xyz/socks5.txt",
+        "https://proxyscan.io/download?type=socks5",
+        "https://proxysearcher.sourceforge.net/Proxy%20List.php?type=socks",
+        "https://proxyspace.pro/socks5.txt",
+        "https://raw.githubusercontent.com/B4RC0DE-TM/proxy-list/main/SOCKS5.txt",
+        "https://raw.githubusercontent.com/hanwayTech/free-proxy-list/main/socks5.txt",
+        "https://raw.githubusercontent.com/hookzof/socks5_list/master/proxy.txt",
+        "https://raw.githubusercontent.com/HyperBeats/proxy-list/main/socks5.txt",
+        "https://raw.githubusercontent.com/jetkai/proxy-list/main/online-proxies/txt/proxies-socks5.txt",
+        "https://raw.githubusercontent.com/manuGMG/proxy-365/main/SOCKS5.txt",
+        "https://raw.githubusercontent.com/mmpx12/proxy-list/master/socks5.txt",
+        "https://raw.githubusercontent.com/ObcbO/getproxy/master/socks5.txt",
+        "https://raw.githubusercontent.com/proxylist-to/proxy-list/main/socks5.txt",
+        "https://raw.githubusercontent.com/roosterkid/openproxylist/main/SOCKS5_RAW.txt",
+        "https://raw.githubusercontent.com/saschazesiger/Free-Proxies/master/proxies/socks5.txt",
+        "https://raw.githubusercontent.com/ShiftyTR/Proxy-List/master/socks5.txt",
+        "https://raw.githubusercontent.com/TheSpeedX/PROXY-List/master/socks5.txt",
+        "https://raw.githubusercontent.com/Zaeem20/FREE_PROXIES_LIST/master/socks5.txt",
+        "https://raw.githubusercontent.com/zevtyardt/proxy-list/main/socks5.txt",
+        "https://www.freeproxychecker.com/result/socks5_proxies.txt"
     ];
 
     let socks_urls = socks_urls.map(|x| x.to_string());
